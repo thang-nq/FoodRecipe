3f5051bd3435390b544b9093ba38335e8b9f2be0
Thang Nguyen <quocthangn1990@gmail.com>
2023-09-10 15:49:37 +0700

    Initial Commit

---
+++ FoodRecipe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodRecipe
+//
+//  Created by Thang Nguyen on 10/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodRecipe/FoodRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeApp.swift
+//  FoodRecipe
+//
+//  Created by Thang Nguyen on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
