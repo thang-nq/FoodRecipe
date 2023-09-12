170ecef1cd9f946f1cee6ec7aa850f1db47532b8
Man Pham <57248833+ManPhamQuang@users.noreply.github.com>
2023-09-12 19:48:15 +0700

    Added Recipe Detail view (#7)
    * init recipe detail view
    
    * added nutritions
    
    * refactor views
    
    * added ingredients view
    
    * added steps
    
    * added sliding tab view
    
    ---------
    
    Co-authored-by: Man Pham <quangman1404@gmail.com>

---
+++ FoodRecipe/Views/RecipeDetail/IngredientsView.swift
@@ -0,0 +1,40 @@
+//
+//  IngredientsView.swift
+//  FoodRecipe
+//
+//  Created by Man Pham on 12/09/2023.
+//
+
+import SwiftUI
+
+
+var MOCK_INGREDIENTS = ["3 green peppers, halved", "1/2 tsp hot smoked paprika", "3 garlic cloves, finely diced", "1 red onion, finely chopped", "corigander, leaves picked and roughly chopped, stems finely chopped"]
+struct IngredientsView: View {
+    var ingredientsList: [String]
+    var body: some View {
+        SectionContainerView {
+//            SectionTitleView(title: "Ingredients")
+            VStack (alignment: .leading) {
+                ForEach(ingredientsList, id: \.self) { ingredient in
+                    HStack {
+                        Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
+                        Text(ingredient)
+                    }
+                    
+                }
+            }
+            
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+        }
+//        .padding(.horizontal, 25)
+    }
+}
+
+struct IngredientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack(alignment: .top) {
+            Color("LightGray").ignoresSafeArea(.all)
+            IngredientsView(ingredientsList: MOCK_INGREDIENTS)
+        }
+    }
+}
---
+++ FoodRecipe/Views/RecipeDetail/SectionContainerView.swift
@@ -0,0 +1,39 @@
+//
+//  SectionContainerView.swift
+//  FoodRecipe
+//
+//  Created by Man Pham on 12/09/2023.
+//
+
+import SwiftUI
+
+
+struct SectionContainerView<Content>: View where Content: View {
+    @State private var content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        
+        VStack(alignment: .center, spacing: 16) {
+            content()
+        }
+        .padding(16)
+        .frame(width: .infinity, alignment: .top)
+        .background(.white)
+        .cornerRadius(16)
+    }
+}
+
+struct SectionContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("LightGray").ignoresSafeArea(.all)
+            SectionContainerView() {
+                Text("Nutritions")
+            }
+        }
+    }
+}
