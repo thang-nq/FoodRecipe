0b156797beaa0226db9dc8449c33e4fda0ed4ecd
1josee <102713181+1josee@users.noreply.github.com>
2023-09-14 22:15:15 +0700

    Add nutrition in create recipe

--- FoodRecipe/Views/CreateRecipe/CreateIntroView.swift
+++ FoodRecipe/Views/CreateRecipe/CreateIntroView.swift
@@ -13,6 +13,14 @@ struct CreateIntroView: View {
     @State private var recipeName = ""
     @State private var minutes = ""
     @State private var description = ""
+    @State private var calories: Int = 0
+    @State private var carb: Int = 0
+    @State private var protein: Int = 0
+    @State private var fat: Int = 0
+    @State private var sugars: Int = 0
+    @State private var salt: Int = 0
+    @State private var saturates: Int = 0
+    @State private var fibre: Int = 0
 
     var body: some View {
         ScrollView{
@@ -60,6 +68,26 @@ struct CreateIntroView: View {
                     .colorMultiply(Color(.systemGray6))
                     .cornerRadius(10)
                     .padding(.horizontal, 15)
+                    .padding(.bottom, 10)
+                HStack{
+                    Text("Nutrition")
+                        .font(.custom("ZillaSlab-SemiBold", size: 22))
+                        .padding(.leading, 15)
+                        .padding(.bottom, 10)
+                    Spacer()
+                }
+                HStack{
+                    NutriotionInput(value: $calories, name: "Calories", placeHolder: "0")
+                    NutriotionInput(value: $carb, name: "Carb", placeHolder: "0")
+                    NutriotionInput(value: $protein, name: "Protein", placeHolder: "0")
+                    NutriotionInput(value: $fat, name: "Fat", placeHolder: "0")
+                }
+                HStack{
+                    NutriotionInput(value: $sugars, name: "Sugars", placeHolder: "0")
+                    NutriotionInput(value: $salt, name: "Salt", placeHolder: "0")
+                    NutriotionInput(value: $saturates, name: "Saturates", placeHolder: "0")
+                    NutriotionInput(value: $fibre, name: "Fibre", placeHolder: "0")
+                }
                 
             }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
@@ -82,6 +110,32 @@ struct InputFieldRecipe: View {
                 TextField(placeHolder, text: $text)
                     .font(.custom("ZillaSlab-Regular", size: 16))
             }
+            
+            Divider()
+        }
+        .padding(.horizontal, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+struct NutriotionInput: View {
+    @Binding var value: Int
+    let name: String
+    let placeHolder: String
+//    var isSecureField = false
+    var body: some View {
+        VStack (alignment: .leading, spacing: 12) {
+            Text(name)
+                .font(.custom("ZillaSlab-Regular", size: 15))
+//            if isSecureField {
+//                SecureField(placeHolder, value: $value)
+//                    .font(.custom("ZillaSlab-Regular", size: 16))
+//            } else {
+//                TextField(placeHolder, value: $value, formatter: NumberFormatter())
+//                    .font(.custom("ZillaSlab-Regular", size: 16))
+//            }
+            TextField(placeHolder, value: $value, formatter: NumberFormatter())
+                .font(.custom("ZillaSlab-Regular", size: 15))
             
             Divider()
         }
