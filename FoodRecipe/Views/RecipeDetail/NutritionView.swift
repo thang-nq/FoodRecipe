fdb7871c7c136923efc5c0ffe0b47aa28b04b8a8
Man Pham <57248833+ManPhamQuang@users.noreply.github.com>
2023-09-16 22:02:12 +0700

    Dev/recipe detail api integrate (#29)
    * integrate api recipe detail: name, nutritions, intro, steps, ingredients
    
    * hide default bar in recipe detail
    
    * add back button function

--- FoodRecipe/Views/RecipeDetail/NutritionView.swift
+++ FoodRecipe/Views/RecipeDetail/NutritionView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct NutritionView: View {
+    var recipe: Recipe
     var body: some View {
         SectionContainerView {
             SectionTitleView(title: "Nutrition")
-            NutritionList
+            ForEach(Array(stride(from: 0, to: recipe.nutritionsArray.count, by: 3)), id: \.self) { index in
+                HStack {
+                    NutritionElementView(item: recipe.nutritionsArray[index])
+                    if(index + 1 < recipe.nutritionsArray.count) {
+                        NutritionElementView(item: recipe.nutritionsArray[index+1])
+                    }
+                    if(index + 2 < recipe.nutritionsArray.count) {
+                        NutritionElementView(item: recipe.nutritionsArray[index+2])
+                    }
+                }
+            }
         }
         .padding(.top, 300)
         .padding(.horizontal, 25)
@@ -22,51 +33,24 @@ struct NutritionView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack(alignment: .top) {
             Color("LightGray").ignoresSafeArea(.all)
-            NutritionView()
+            NutritionView(
+                recipe: Recipe(name: "Chicken", creatorID: "820450349", protein: 200, fat: 80, sugars: 20, salt: 30 )
+            )
         }
     }
 }
 
-// MARK: Nutrition List
-var NutritionList: some View {
-    HStack {
+
+struct NutritionElementView: View {
+    var item: NutritionItem
+    
+    var body: some View {
         VStack (spacing: 7){
-            Text("kcal")
+            Text(item.type)
                 .font(.system(size: 18))
                 .foregroundColor(Color.theme.White)
                 .bold()
-            Text("256")
-                .font(.system(size: 22))
-                .foregroundColor(Color.theme.White)
-                .fontWeight(.semibold)
-        }
-        .padding(10)
-        .frame(width: 100, height: 80)
-        .background(Color.theme.LightOrange)
-        .cornerRadius(10)
-        
-        VStack (spacing: 7){
-            Text("carbs")
-                .font(.system(size: 18))
-                .foregroundColor(Color.theme.White)
-                .bold()
-            Text("256")
-                .font(.system(size: 22))
-                .foregroundColor(Color.theme.White)
-                .fontWeight(.semibold)
-        }
-        .padding(10)
-        .frame(width: 100, height: 80)
-        .background(Color.theme.LightOrange)
-        .cornerRadius(10)
-        
-        
-        VStack (spacing: 7){
-            Text("fibre")
-                .font(.system(size: 18))
-                .foregroundColor(Color.theme.White)
-                .bold()
-            Text("16g")
+            Text("\(item.value)")
                 .font(.system(size: 22))
                 .foregroundColor(Color.theme.White)
                 .fontWeight(.semibold)
@@ -77,3 +61,4 @@ var NutritionList: some View {
         .cornerRadius(10)
     }
 }
+
--- FoodRecipe/Views/RecipeDetail/StepsView.swift
+++ FoodRecipe/Views/RecipeDetail/StepsView.swift
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-var MOCK_STEPS = ["Setelah dicuci bersih, rebus sayap ayam hingga matang dengan 500 ml air. Buang busanya dan saring untuk dijadikan kaldu. ",
-                  "Tumis bawang putih, bawang merah dan daun bawang hingga harum. Masukkan pala dan lada bubuk, tumis sebentar. Matikan api. ",
-                  "3 garlic cloves, finely diced",
-                  "Rebus lagi air kaldu, masukkan makaroni dan masak hingga setengah matang. Masukkan wortel dan masak hingga mendidih. ",
-                  "Masukkan bumbu dan daun seledri. Masak hingga semua bahan matang dan kuah sedap."]
+//var MOCK_STEPS = ["Setelah dicuci bersih, rebus sayap ayam hingga matang dengan 500 ml air. Buang busanya dan saring untuk dijadikan kaldu. ",
+//                  "Tumis bawang putih, bawang merah dan daun bawang hingga harum. Masukkan pala dan lada bubuk, tumis sebentar. Matikan api. ",
+//                  "3 garlic cloves, finely diced",
+//                  "Rebus lagi air kaldu, masukkan makaroni dan masak hingga setengah matang. Masukkan wortel dan masak hingga mendidih. ",
+//                  "Masukkan bumbu dan daun seledri. Masak hingga semua bahan matang dan kuah sedap."]
+var MOCK_STEPS = [CookingStep(context: "Do this and that", backgroundURL: "", stepNumber: 1)]
 struct StepsView: View {
-    var stepsList: [String]
+    var stepsList: [CookingStep]
     var body: some View {
         SectionContainerView {
 //            SectionTitleView(title: "Steps")
             VStack (alignment: .leading) {
-                ForEach(stepsList, id: \.self) { step in
+                ForEach(stepsList) { step in
                     HStack {
                         Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
-                        Text(step)
+                        Text(step.context)
                     }
                     
                 }
