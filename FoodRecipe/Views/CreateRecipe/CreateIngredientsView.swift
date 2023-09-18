44355ddf00ca056234f602359042ff38a3daffcc
1josee <102713181+1josee@users.noreply.github.com>
2023-09-18 16:51:18 +0700

    Restructure create recipe code (#46)
    * Add comment for function
    
    * Restructure thecreate recipe view and comment
    
    * Restructure thecreate intro view and comment
    
    * Restructure code in create ingredients view and add comment
    
    * Restructure code in create steps view and add comment
    
    * add accessbility label for create recipe
    
    * add more comment for create recipe view

--- FoodRecipe/Views/CreateRecipe/CreateIngredientsView.swift
+++ FoodRecipe/Views/CreateRecipe/CreateIngredientsView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct CreateIngredientsView: View {
+    // MARK: VARIABLES
     @State private var showingSheet = false
     @State private var InputIngredient = ""
     @Binding var Ingredients: [String]
     
+    // MARK: FUNCTION
     // Remove Ingredient function
     private func removeIngredient(_ ingredient: String) {
         if let index = Ingredients.firstIndex(of: ingredient) {
@@ -20,48 +22,21 @@ struct CreateIngredientsView: View {
     }
     
     var body: some View {
+        // MARK: MAIN LAYOUT
         VStack{
             ScrollView{
-                VStack(alignment: .leading) {
-                    if(Ingredients.isEmpty){
-                        HStack {
-                            Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
-                            Text("Click the plus button below to adding ingredient")
-                                .font(.custom("ZillaSlab-Regular", size: 20))
-                                
-                        }.padding(.leading, 20)
-                    }
-                    ForEach(Ingredients, id: \.self) { ingredient in
-                        HStack() {
-                            Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
-                            Text(ingredient)
-                                .font(.custom("ZillaSlab-Regular", size: 20))
-                                .frame(width: 280, alignment: .leading)
-                            Spacer()
-                            Button(action: {
-                                removeIngredient(ingredient)
-                            }) {
-                                Image(systemName: "minus.circle")
-                                    .resizable()
-                                    .frame(width: 25, height: 25)
-                                    .foregroundColor(Color("Orange"))
-                                    .padding(.trailing, 15)
-                            }
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 20)
-                    }
-                    Spacer()
-                }
-                
+                    ingredients
+                        .accessibilityLabel("Ingredients")
             }
         }
         .sheet(isPresented: $showingSheet){
+            // MARK: ADD INGREDIENTS SHEET
             AddIngredientsSheetView(InputIngredient: $InputIngredient, Ingredients: $Ingredients)
                 .presentationDetents([.height(300)])
         }
         .frame(maxWidth: 500, maxHeight: .infinity, alignment: .topLeading)
         .overlay(
+            // MARK: Create new ingredient button
             Button(action: {
                 self.showingSheet.toggle()
             }, label: {
@@ -73,15 +48,14 @@ struct CreateIngredientsView: View {
                                 .clipShape(Circle())
                                 
             })
-            
             .modifier(ButtonModifier()),
             alignment: .bottomTrailing
-            
         )
         
     }
 }
 
+// Button modifier
 struct ButtonModifier: ViewModifier {
   func body(content: Content) -> some View {
     content
@@ -91,6 +65,7 @@ struct ButtonModifier: ViewModifier {
   }
 }
 
+// MARK: ADD INGREDIENTS SHEET VIEW
 struct AddIngredientsSheetView: View {
     @Binding var InputIngredient : String
     @Binding var Ingredients : [String]
@@ -121,3 +96,39 @@ struct CreateIngredientsView_Previews: PreviewProvider {
     }
 }
 
+private extension CreateIngredientsView{
+    // MARK: INGREDIENTS UI
+    var ingredients: some View{
+        VStack(alignment: .leading) {
+            if(Ingredients.isEmpty){
+                HStack {
+                    Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
+                    Text("Click the plus button below to adding ingredient")
+                        .font(.custom("ZillaSlab-Regular", size: 20))
+                        
+                }.padding(.leading, 20)
+            }
+            ForEach(Ingredients, id: \.self) { ingredient in
+                HStack() {
+                    Circle().fill(Color.theme.Orange).frame(width: 10, height: 10)
+                    Text(ingredient)
+                        .font(.custom("ZillaSlab-Regular", size: 20))
+                        .frame(width: 280, alignment: .leading)
+                    Spacer()
+                    Button(action: {
+                        removeIngredient(ingredient)
+                    }) {
+                        Image(systemName: "minus.circle")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color("Orange"))
+                            .padding(.trailing, 15)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+            }
+            Spacer()
+        }
+    }
+}
--- FoodRecipe/Views/CreateRecipe/CreateRecipeView.swift
+++ FoodRecipe/Views/CreateRecipe/CreateRecipeView.swift
@@ -10,6 +10,7 @@ import SlidingTabView
 import PhotosUI
 
 struct CreateRecipeView: View {
+    //MARK: VARIABLES
     @StateObject var homeVM = HomeViewModel()
     @State private var backgroundPhoto: PhotosPickerItem? = nil
     @State private var recipeName = ""
@@ -24,19 +25,16 @@ struct CreateRecipeView: View {
     @State private var salt: Int = 0
     @State private var saturates: Int = 0
     @State private var fibre: Int = 0
-    
     @State private var currentSelectedTags: [String] = []
     @State private var currentSelectedMealTypes: [String] = []
     @State private var currentMealType : String = ""
-    
     @State private var Ingredients: [String] = []
-    
     @State private var Steps: [String] = []
     @State private var listStepsPhoto: [PhotosPickerItem?] = []
-    
     @State private var cookingSteps: [CookingStepInterface] = []
     @State private var recipeValidated: Bool = false
-    
+    @State private var selectedTabIndex = 0
+    @State private var isLoading = false
     //MARK: POP UP VARIABLES
     @State var showPopUp = false
     @State var popUpIcon = ""
@@ -44,22 +42,8 @@ struct CreateRecipeView: View {
     @State var popUpContent = ""
     @State var popUpIconColor = Color.theme.BlueInstance
     
-    @State private var selectedTabIndex = 0
-    @State private var isLoading = false
-//    func addingCookingSteps (){
-//
-//        for index in 0..<Steps.count {
-//            let context = Steps[index]
-//            var imageData: PhotosPickerItem? = nil
-//
-//            if index < listStepsPhoto.count {
-//                imageData = listStepsPhoto[index]
-//            }
-//
-//            let cookingStep = CookingStepInterface(context: context, imageData: imageData, stepNumber: index + 1)
-//            cookingSteps.append(cookingStep)
-//        }
-//    }
+    //MARK: FUNCTION
+    // Get meal type function
     func getMealType(){
         if(currentSelectedMealTypes.isEmpty){
             currentMealType = ""
@@ -68,7 +52,8 @@ struct CreateRecipeView: View {
             currentMealType = mealType
         }
     }
-    //Adding Cooking Steps function
+    
+    // Adding Cooking Steps with photo function
     func addingCookingSteps() async{
         for index in 0..<Steps.count {
             let context = Steps[index]
@@ -88,6 +73,8 @@ struct CreateRecipeView: View {
             cookingSteps.append(cookingStep)
         }
     }
+    
+    // Show success pop up function
     func showSuccessPopup() async{
         showPopUp = true
         popUpIcon = "checkmark.message.fill"
@@ -95,6 +82,8 @@ struct CreateRecipeView: View {
         popUpContent = "You can check your recipe in the My Recipe section"
         popUpIconColor = Color.theme.GreenInstance
     }
+    
+    // Reset recipe function
     func resetTheCreateRecipeForm() async{
         backgroundPhoto = nil
         recipeName = ""
@@ -118,84 +107,33 @@ struct CreateRecipeView: View {
         cookingSteps = []
         recipeValidated = false
     }
+    
+    // Loading
     func loading() async{
         isLoading = true
     }
+    
+    // Cancel loading
     func cancelLoading() async{
         isLoading = false
     }
+    
     var body: some View {
+        
+        //MARK: MAIN LAYOUT
         ZStack{
+            
             VStack {
-                HStack {
-                    Spacer()
-                    Text("Create new recipe")
-                        .font(.custom("ZillaSlab-Bold", size: 25))
-                        .padding(.leading, 70)
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        getMealType()
-                        if recipeName.isEmpty || cookingTime == 0 || servingSize == 0 || backgroundPhoto == nil || description.isEmpty || Ingredients.isEmpty || Steps.isEmpty || currentMealType.isEmpty || currentSelectedTags.isEmpty || Steps.isEmpty {
-                            showPopUp = true
-                            popUpIcon = "xmark"
-                            popUptitle = "Missing Information"
-                            popUpContent = "Please fill in all fields in Intro, Ingredients, Steps."
-                            popUpIconColor = Color.theme.RedInstance
-                        } else{
-                            recipeValidated = true
-                        }
-                        if (recipeValidated == true){
-                            Task {
-                                await loading()
-                                await addingCookingSteps()
-                                try await homeVM.addRecipe(recipe: Recipe(name: recipeName,
-                                                                          creatorID: "99",
-                                                                          mealType: currentMealType,
-                                                                          intro: description,
-                                                                          servingSize: servingSize,
-                                                                          cookingTime: cookingTime,
-                                                                          calories: calories,
-                                                                          carb: carb,
-                                                                          protein: protein,
-                                                                          fat: fat,
-                                                                          sugars: sugars,
-                                                                          salt: salt,
-                                                                          saturates: saturates,
-                                                                          fibre: fibre,
-                                                                          ingredients: Ingredients,
-                                                                          tags: currentSelectedTags),
-                                                           image: backgroundPhoto,
-                                                           cookingSteps: cookingSteps
-                                )
-                                await resetTheCreateRecipeForm()
-                                await cancelLoading()
-                                await showSuccessPopup()
-                            }
-                        }
-                    }) {
-                        Text("Create")
-                            .font(.system(size: 20))
-                        
-                    }.padding(.trailing, 20)
-                }
-                SlidingTabView(selection: self.$selectedTabIndex, tabs: ["Intro","Ingredients", "Steps"], font: .custom("ZillaSlab-Regular", size: 22),  activeAccentColor: Color.theme.Orange, selectionBarColor: Color.theme.Orange)
                 
-                if selectedTabIndex == 0 {
-                    CreateIntroView(backgroundPhoto: $backgroundPhoto ,recipeName: $recipeName, cookingTime: $cookingTime, servingSize: $servingSize, description: $description, calories: $calories, carb: $carb, protein: $protein, fat: $fat, sugars: $sugars, salt: $salt, saturates: $saturates, fibre: $fibre, currentSelectedTags: $currentSelectedTags, currentSelectedMealTypes: $currentSelectedMealTypes)
-                }
+                topBar
+                    .accessibilityLabel("Top bar")
                 
-                if selectedTabIndex == 1 {
-                    CreateIngredientsView(Ingredients: $Ingredients)
-                }
-                
-                if selectedTabIndex == 2 {
-                    CreateStepsView(Steps: $Steps, listStepsPhoto: $listStepsPhoto, backgroundPhoto: $backgroundPhoto)
-                }
+                slidingTab
+                    .accessibilityLabel("Sliding tab")
                 
             }
             .overlay(
+            // MARK: SHOW THE SUCCESS POP UP
                 ZStack {
                     if showPopUp {
                         Color.theme.DarkWhite.opacity(0.5)
@@ -206,6 +144,8 @@ struct CreateRecipeView: View {
                     .opacity(showPopUp ? 1 : 0)
             )
             .background(Color.theme.White)
+            
+            // MARK: CHECK LOADING
             if (isLoading == true){
                 ZStack {
                     Color(.black)
@@ -226,3 +166,86 @@ struct CreateRecipeView_Previews: PreviewProvider {
     }
 }
 
+private extension CreateRecipeView{
+    
+    //MARK: TOP BAR UI
+    var topBar: some View{
+        HStack {
+            Spacer()
+            // Title of the view
+            Text("Create new recipe")
+                .font(.custom("ZillaSlab-Bold", size: 25))
+                .padding(.leading, 70)
+            
+            Spacer()
+            
+            // Button create new recipe
+            Button(action: {
+                getMealType()
+                
+                // Validate input field
+                if recipeName.isEmpty || cookingTime == 0 || servingSize == 0 || backgroundPhoto == nil || description.isEmpty || Ingredients.isEmpty || Steps.isEmpty || currentMealType.isEmpty || currentSelectedTags.isEmpty || Steps.isEmpty {
+                    showPopUp = true
+                    popUpIcon = "xmark"
+                    popUptitle = "Missing Information"
+                    popUpContent = "Please fill in all fields in Intro, Ingredients, Steps."
+                    popUpIconColor = Color.theme.RedInstance
+                } else{
+                    recipeValidated = true
+                }
+                
+                // Creating recipe when the inputs are validated
+                if (recipeValidated == true){
+                    Task {
+                        await loading()
+                        await addingCookingSteps()
+                        try await homeVM.addRecipe(recipe: Recipe(name: recipeName,
+                                                                  creatorID: "99",
+                                                                  mealType: currentMealType,
+                                                                  intro: description,
+                                                                  servingSize: servingSize,
+                                                                  cookingTime: cookingTime,
+                                                                  calories: calories,
+                                                                  carb: carb,
+                                                                  protein: protein,
+                                                                  fat: fat,
+                                                                  sugars: sugars,
+                                                                  salt: salt,
+                                                                  saturates: saturates,
+                                                                  fibre: fibre,
+                                                                  ingredients: Ingredients,
+                                                                  tags: currentSelectedTags),
+                                                   image: backgroundPhoto,
+                                                   cookingSteps: cookingSteps
+                        )
+                        await resetTheCreateRecipeForm()
+                        await cancelLoading()
+                        await showSuccessPopup()
+                    }
+                }
+            }) {
+                Text("Create")
+                    .font(.system(size: 20))
+                
+            }.padding(.trailing, 20)
+        }
+    }
+    
+    //MARK: SLIDING TAB UI
+    var slidingTab: some View{
+        VStack{
+            SlidingTabView(selection: self.$selectedTabIndex, tabs: ["Intro","Ingredients", "Steps"], font: .custom("ZillaSlab-Regular", size: 22),  activeAccentColor: Color.theme.Orange, selectionBarColor: Color.theme.Orange)
+            // Check the selected tab index
+            if selectedTabIndex == 0 {
+                CreateIntroView(backgroundPhoto: $backgroundPhoto ,recipeName: $recipeName, cookingTime: $cookingTime, servingSize: $servingSize, description: $description, calories: $calories, carb: $carb, protein: $protein, fat: $fat, sugars: $sugars, salt: $salt, saturates: $saturates, fibre: $fibre, currentSelectedTags: $currentSelectedTags, currentSelectedMealTypes: $currentSelectedMealTypes)
+            }
+            if selectedTabIndex == 1 {
+                CreateIngredientsView(Ingredients: $Ingredients)
+            }
+            if selectedTabIndex == 2 {
+                CreateStepsView(Steps: $Steps, listStepsPhoto: $listStepsPhoto, backgroundPhoto: $backgroundPhoto)
+            }
+        }
+    }
+    
+}
