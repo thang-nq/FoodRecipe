80ffc951398ee8ee7447490dbefc651a7fcdbd96
1josee <trnnhattien@gmail.com>
2023-09-18 18:38:52 +0700

    Create CreateRecipe View Model

--- FoodRecipe/Views/CreateRecipe/CreateRecipeView.swift
+++ FoodRecipe/Views/CreateRecipe/CreateRecipeView.swift
@@ -11,157 +11,157 @@ import PhotosUI
 
 struct CreateRecipeView: View {
     //MARK: VARIABLES
-    @StateObject var homeVM = HomeViewModel()
-    @State private var backgroundPhoto: PhotosPickerItem? = nil
-    @State private var recipeName = ""
-    @State private var cookingTime : Int = 0
-    @State private var servingSize : Int = 0
-    @State private var description = ""
-    @State private var calories: Int = 0
-    @State private var carb: Int = 0
-    @State private var protein: Int = 0
-    @State private var fat: Int = 0
-    @State private var sugars: Int = 0
-    @State private var salt: Int = 0
-    @State private var saturates: Int = 0
-    @State private var fibre: Int = 0
-    @State private var currentSelectedTags: [String] = []
-    @State private var currentSelectedMealTypes: [String] = []
-    @State private var currentMealType : String = ""
-    @State private var Ingredients: [String] = []
-    @State private var Steps: [String] = []
-    @State private var listStepsPhoto: [PhotosPickerItem?] = []
-    @State private var cookingSteps: [CookingStepInterface] = []
-    @State private var recipeValidated: Bool = false
-    @State private var selectedTabIndex = 0
-    @State private var isLoading = false
-    //MARK: POP UP VARIABLES
-    @State var showPopUp = false
-    @State var popUpIcon = ""
-    @State var popUptitle = ""
-    @State var popUpContent = ""
-    @State var popUpIconColor = Color.theme.BlueInstance
-    
-    //MARK: FUNCTION
-    // Get meal type function
-    func getMealType(){
-        if(currentSelectedMealTypes.isEmpty){
-            currentMealType = ""
-        }else {
-            let mealType = currentSelectedMealTypes[0]
-            currentMealType = mealType
-        }
-    }
-    
-    // Adding Cooking Steps with photo function
-    func addingCookingSteps() async{
-        for index in 0..<Steps.count {
-            let context = Steps[index]
-            var imageData: PhotosPickerItem? = nil
-            
-            if index < listStepsPhoto.count {
-                if let photo = listStepsPhoto[index] {
-                    imageData = photo
-                } else {
-                    imageData = backgroundPhoto
-                }
-            } else {
-                imageData = backgroundPhoto
-            }
-            
-            let cookingStep = CookingStepInterface(context: context, imageData: imageData, stepNumber: index + 1)
-            cookingSteps.append(cookingStep)
-        }
-    }
-    
-    // Show success pop up function
-    func showSuccessPopup() async{
-        showPopUp = true
-        popUpIcon = "checkmark.message.fill"
-        popUptitle = "Create recipe success"
-        popUpContent = "You can check your recipe in the My Recipe section"
-        popUpIconColor = Color.theme.GreenInstance
-    }
-    
-    // Reset recipe function
-    func resetTheCreateRecipeForm() async{
-        backgroundPhoto = nil
-        recipeName = ""
-        cookingTime = 0
-        servingSize = 0
-        description = ""
-        calories = 0
-        carb = 0
-        protein = 0
-        fat = 0
-        sugars = 0
-        salt = 0
-        saturates = 0
-        fibre = 0
-        currentSelectedTags = []
-        currentSelectedMealTypes = []
-        currentMealType = ""
-        Ingredients = []
-        Steps = []
-        listStepsPhoto = []
-        cookingSteps = []
-        recipeValidated = false
-    }
-    
-    // Loading
-    func loading() async{
-        isLoading = true
-    }
-    
-    // Cancel loading
-    func cancelLoading() async{
-        isLoading = false
-    }
-    
-    // Create recipe function
-    func createRecipe(){
-        getMealType()
-        // Validate input field
-        if recipeName.isEmpty || cookingTime == 0 || servingSize == 0 || backgroundPhoto == nil || description.isEmpty || Ingredients.isEmpty || Steps.isEmpty || currentMealType.isEmpty || calories == 0 {
-            showPopUp = true
-            popUpIcon = "xmark"
-            popUptitle = "Missing Information"
-            popUpContent = "Please fill in all fields in Intro, Ingredients, Steps."
-            popUpIconColor = Color.theme.RedInstance
-        } else{
-            recipeValidated = true
-        }
-        
-        // Creating recipe when the inputs are validated
-        if (recipeValidated == true){
-            Task {
-                await loading()
-                await addingCookingSteps()
-                try await homeVM.addRecipe(recipe: Recipe(name: recipeName,
-                                                          creatorID: "99",
-                                                          mealType: currentMealType,
-                                                          intro: description,
-                                                          servingSize: servingSize,
-                                                          cookingTime: cookingTime,
-                                                          calories: calories,
-                                                          carb: carb,
-                                                          protein: protein,
-                                                          fat: fat,
-                                                          sugars: sugars,
-                                                          salt: salt,
-                                                          saturates: saturates,
-                                                          fibre: fibre,
-                                                          ingredients: Ingredients,
-                                                          tags: currentSelectedTags),
-                                           image: backgroundPhoto,
-                                           cookingSteps: cookingSteps
-                )
-                await resetTheCreateRecipeForm()
-                await cancelLoading()
-                await showSuccessPopup()
-            }
-        }
-    }
+    @StateObject var createRecipeVM = CreateRecipeViewModel()
+//    @State private var backgroundPhoto: PhotosPickerItem? = nil
+//    @State private var recipeName = ""
+//    @State private var cookingTime : Int = 0
+//    @State private var servingSize : Int = 0
+//    @State private var description = ""
+//    @State private var calories: Int = 0
+//    @State private var carb: Int = 0
+//    @State private var protein: Int = 0
+//    @State private var fat: Int = 0
+//    @State private var sugars: Int = 0
+//    @State private var salt: Int = 0
+//    @State private var saturates: Int = 0
+//    @State private var fibre: Int = 0
+//    @State private var currentSelectedTags: [String] = []
+//    @State private var currentSelectedMealTypes: [String] = []
+//    @State private var currentMealType : String = ""
+//    @State private var Ingredients: [String] = []
+//    @State private var Steps: [String] = []
+//    @State private var listStepsPhoto: [PhotosPickerItem?] = []
+//    @State private var cookingSteps: [CookingStepInterface] = []
+//    @State private var recipeValidated: Bool = false
+//    @State private var selectedTabIndex = 0
+//    @State private var isLoading = false
+//    //MARK: POP UP VARIABLES
+//    @State var showPopUp = false
+//    @State var popUpIcon = ""
+//    @State var popUptitle = ""
+//    @State var popUpContent = ""
+//    @State var popUpIconColor = Color.theme.BlueInstance
+//
+//    //MARK: FUNCTION
+//    // Get meal type function
+//    func getMealType(){
+//        if(currentSelectedMealTypes.isEmpty){
+//            currentMealType = ""
+//        }else {
+//            let mealType = currentSelectedMealTypes[0]
+//            currentMealType = mealType
+//        }
+//    }
+//
+//    // Adding Cooking Steps with photo function
+//    func addingCookingSteps() async{
+//        for index in 0..<Steps.count {
+//            let context = Steps[index]
+//            var imageData: PhotosPickerItem? = nil
+//
+//            if index < listStepsPhoto.count {
+//                if let photo = listStepsPhoto[index] {
+//                    imageData = photo
+//                } else {
+//                    imageData = backgroundPhoto
+//                }
+//            } else {
+//                imageData = backgroundPhoto
+//            }
+//
+//            let cookingStep = CookingStepInterface(context: context, imageData: imageData, stepNumber: index + 1)
+//            cookingSteps.append(cookingStep)
+//        }
+//    }
+//
+//    // Show success pop up function
+//    func showSuccessPopup() async{
+//        showPopUp = true
+//        popUpIcon = "checkmark.message.fill"
+//        popUptitle = "Create recipe success"
+//        popUpContent = "You can check your recipe in the My Recipe section"
+//        popUpIconColor = Color.theme.GreenInstance
+//    }
+//
+//    // Reset recipe function
+//    func resetTheCreateRecipeForm() async{
+//        backgroundPhoto = nil
+//        recipeName = ""
+//        cookingTime = 0
+//        servingSize = 0
+//        description = ""
+//        calories = 0
+//        carb = 0
+//        protein = 0
+//        fat = 0
+//        sugars = 0
+//        salt = 0
+//        saturates = 0
+//        fibre = 0
+//        currentSelectedTags = []
+//        currentSelectedMealTypes = []
+//        currentMealType = ""
+//        Ingredients = []
+//        Steps = []
+//        listStepsPhoto = []
+//        cookingSteps = []
+//        recipeValidated = false
+//    }
+//
+//    // Loading
+//    func loading() async{
+//        isLoading = true
+//    }
+//
+//    // Cancel loading
+//    func cancelLoading() async{
+//        isLoading = false
+//    }
+//
+//    // Create recipe function
+//    func createRecipe(){
+//        getMealType()
+//        // Validate input field
+//        if recipeName.isEmpty || cookingTime == 0 || servingSize == 0 || backgroundPhoto == nil || description.isEmpty || Ingredients.isEmpty || Steps.isEmpty || currentMealType.isEmpty || calories == 0 {
+//            showPopUp = true
+//            popUpIcon = "xmark"
+//            popUptitle = "Missing Information"
+//            popUpContent = "Please fill in all fields in Intro, Ingredients, Steps."
+//            popUpIconColor = Color.theme.RedInstance
+//        } else{
+//            recipeValidated = true
+//        }
+//
+//        // Creating recipe when the inputs are validated
+//        if (recipeValidated == true){
+//            Task {
+//                await loading()
+//                await addingCookingSteps()
+//                try await homeVM.addRecipe(recipe: Recipe(name: recipeName,
+//                                                          creatorID: "99",
+//                                                          mealType: currentMealType,
+//                                                          intro: description,
+//                                                          servingSize: servingSize,
+//                                                          cookingTime: cookingTime,
+//                                                          calories: calories,
+//                                                          carb: carb,
+//                                                          protein: protein,
+//                                                          fat: fat,
+//                                                          sugars: sugars,
+//                                                          salt: salt,
+//                                                          saturates: saturates,
+//                                                          fibre: fibre,
+//                                                          ingredients: Ingredients,
+//                                                          tags: currentSelectedTags),
+//                                           image: backgroundPhoto,
+//                                           cookingSteps: cookingSteps
+//                )
+//                await resetTheCreateRecipeForm()
+//                await cancelLoading()
+//                await showSuccessPopup()
+//            }
+//        }
+//    }
     
     var body: some View {
         
@@ -180,18 +180,18 @@ struct CreateRecipeView: View {
             .overlay(
             // MARK: SHOW THE SUCCESS POP UP
                 ZStack {
-                    if showPopUp {
+                    if createRecipeVM.showPopUp {
                         Color.theme.DarkWhite.opacity(0.5)
                             .edgesIgnoringSafeArea(.all)
-                        PopUp(iconName: popUpIcon , title: popUptitle, content: popUpContent, iconColor: popUpIconColor ,didClose: {showPopUp = false})
+                        PopUp(iconName: createRecipeVM.popUpIcon , title: createRecipeVM.popUptitle, content: createRecipeVM.popUpContent, iconColor: createRecipeVM.popUpIconColor ,didClose: {createRecipeVM.showPopUp = false})
                     }
                 }
-                    .opacity(showPopUp ? 1 : 0)
+                    .opacity(createRecipeVM.showPopUp ? 1 : 0)
             )
             .background(Color.theme.White)
             
             // MARK: CHECK LOADING
-            if (isLoading == true){
+            if (createRecipeVM.isLoading == true){
                 ZStack {
                     Color(.black)
                         .ignoresSafeArea()
@@ -227,7 +227,7 @@ private extension CreateRecipeView{
             // Button create new recipe
             Button(action: {
                 // Create recipe
-                createRecipe()
+                createRecipeVM.createRecipe()
             }) {
                 Text("Create")
                     .font(.system(size: 20))
@@ -239,16 +239,16 @@ private extension CreateRecipeView{
     //MARK: SLIDING TAB UI
     var slidingTab: some View{
         VStack{
-            SlidingTabView(selection: self.$selectedTabIndex, tabs: ["Intro","Ingredients", "Steps"], font: .custom("ZillaSlab-Regular", size: 22),  activeAccentColor: Color.theme.Orange, selectionBarColor: Color.theme.Orange)
+            SlidingTabView(selection: $createRecipeVM.selectedTabIndex, tabs: ["Intro","Ingredients", "Steps"], font: .custom("ZillaSlab-Regular", size: 22),  activeAccentColor: Color.theme.Orange, selectionBarColor: Color.theme.Orange)
             // Check the selected tab index
-            if selectedTabIndex == 0 {
-                CreateIntroView(backgroundPhoto: $backgroundPhoto ,recipeName: $recipeName, cookingTime: $cookingTime, servingSize: $servingSize, description: $description, calories: $calories, carb: $carb, protein: $protein, fat: $fat, sugars: $sugars, salt: $salt, saturates: $saturates, fibre: $fibre, currentSelectedTags: $currentSelectedTags, currentSelectedMealTypes: $currentSelectedMealTypes)
+            if createRecipeVM.selectedTabIndex == 0 {
+                CreateIntroView(backgroundPhoto: $createRecipeVM.backgroundPhoto ,recipeName: $createRecipeVM.recipeName, cookingTime: $createRecipeVM.cookingTime, servingSize: $createRecipeVM.servingSize, description: $createRecipeVM.description, calories: $createRecipeVM.calories, carb: $createRecipeVM.carb, protein: $createRecipeVM.protein, fat: $createRecipeVM.fat, sugars: $createRecipeVM.sugars, salt: $createRecipeVM.salt, saturates: $createRecipeVM.saturates, fibre: $createRecipeVM.fibre, currentSelectedTags: $createRecipeVM.currentSelectedTags, currentSelectedMealTypes: $createRecipeVM.currentSelectedMealTypes)
             }
-            if selectedTabIndex == 1 {
-                CreateIngredientsView(Ingredients: $Ingredients)
+            if createRecipeVM.selectedTabIndex == 1 {
+                CreateIngredientsView(Ingredients: $createRecipeVM.Ingredients)
             }
-            if selectedTabIndex == 2 {
-                CreateStepsView(Steps: $Steps, listStepsPhoto: $listStepsPhoto, backgroundPhoto: $backgroundPhoto)
+            if createRecipeVM.selectedTabIndex == 2 {
+                CreateStepsView(Steps: $createRecipeVM.Steps, listStepsPhoto: $createRecipeVM.listStepsPhoto, backgroundPhoto: $createRecipeVM.backgroundPhoto)
             }
         }
     }
