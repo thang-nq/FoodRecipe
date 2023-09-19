2f3d2f97645916bf09878e1c8c7f242c975de2f2
1josee <trnnhattien@gmail.com>
2023-09-19 21:41:56 +0700

    Add edit button for create ingredient and create step

--- FoodRecipe/Views/CreateRecipe/CreateStepsView.swift
+++ FoodRecipe/Views/CreateRecipe/CreateStepsView.swift
@@ -15,8 +15,21 @@ struct CreateStepsView: View {
     @Binding var backgroundPhoto: PhotosPickerItem?
     @State private var stepPhoto: PhotosPickerItem? = nil
     @State private var showingSheet = false
+    @State private var showingUpdateSheet = false
     @State private var InputStep = ""
+    @State private var updateStep = ""
+    @State private var updatePhoto : PhotosPickerItem? = nil
+    @State private var showPopUp = false
+    @State private var popUpIcon = ""
+    @State private var popUptitle = ""
+    @State private var popUpContent = ""
+    @State private var popUpIconColor = Color.theme.BlueInstance
     
+    private func updateStepFunction(step: String, photo: PhotosPickerItem) {
+        updateStep = step
+        updatePhoto = photo
+        showingUpdateSheet.toggle()
+    }
     var body: some View {
         //MARK: MAIN LAYOUT
         
@@ -31,11 +44,35 @@ struct CreateStepsView: View {
             AddStepsSheetView(InputStep: $InputStep, Steps: $Steps, stepPhoto: $stepPhoto, listStepsPhoto: $listStepsPhoto, backgroundPhoto: $backgroundPhoto)
                 .presentationDetents([.height(300)])
         }
+        .sheet(isPresented: $showingUpdateSheet){
+            // MARK: UPDATE STEP SHEET UI
+            UpdateStepsSheetView(Steps: $Steps, listStepsPhoto: $listStepsPhoto,updateStep: $updateStep, updatePhoto: $updatePhoto, showingUpdateSheet: $showingUpdateSheet)
+                .presentationDetents([.height(300)])
+        }
         .frame(maxWidth: 500, maxHeight: .infinity, alignment: .topLeading)
+        .overlay(
+        // MARK: SHOW THE SUCCESS POP UP
+            ZStack {
+                if showPopUp {
+                    Color.theme.DarkWhite.opacity(0.5)
+                        .edgesIgnoringSafeArea(.all)
+                    PopUp(iconName: popUpIcon , title: popUptitle, content: popUpContent, iconColor: popUpIconColor ,didClose: {showPopUp = false})
+                }
+            }
+                .opacity(showPopUp ? 1 : 0)
+        )
         .overlay(
             //MARK: ADD STEPS BUTTON
             Button(action: {
-                self.showingSheet.toggle()
+                if backgroundPhoto == nil{
+                    showPopUp = true
+                    popUpIcon = "xmark"
+                    popUptitle = "Missing Recipe Photo"
+                    popUpContent = "Please choose the photo for the recipe in the Intro Section"
+                    popUpIconColor = Color.theme.RedInstance
+                } else{
+                    self.showingSheet.toggle()
+                }
             }, label: {
                 Image(systemName: "plus")
                                 .font(.system(size: 24))
@@ -77,10 +114,53 @@ struct AddStepsSheetView: View {
                 if let photo = stepPhoto {
                     listStepsPhoto.append(photo)
                 } else{
-                    listStepsPhoto.append(nil)
+                    listStepsPhoto.append(backgroundPhoto)
                 }
                 stepPhoto = nil
                 InputStep = ""
+            }) {
+                Text("Save")
+                    .foregroundColor(Color.theme.WhiteInstance)
+                                    .font(.headline)
+                                    .frame(width: 120, height: 40)
+                                    .background(Color.theme.OrangeInstance)
+                                    .cornerRadius(8)
+            }
+        }
+}
+
+//MARK: UPDATE STEP SHEET VIEW
+struct UpdateStepsSheetView: View {
+    @State private var newStep : String = ""
+    @State private var newStepPhoto : PhotosPickerItem? = nil
+    @Binding var Steps : [String]
+    @Binding var listStepsPhoto : [PhotosPickerItem?]
+    @Binding var updateStep : String
+    @Binding var updatePhoto : PhotosPickerItem?
+    @Binding var showingUpdateSheet : Bool
+    var body: some View{
+        VStack{
+            InputFieldRecipe(text: $newStep , title: "Update Step", placeHolder: "Enter new step")
+        }
+        PhotosPicker(selection: $newStepPhoto, photoLibrary: .shared()) {
+            Label("Select new a photo for step", systemImage: "photo.fill")
+        }
+        .padding(.vertical, 10)
+        .foregroundColor(Color.theme.OrangeInstance)
+            Button(action: {
+                if(!newStep.isEmpty){
+                    if let index = Steps.firstIndex(of: updateStep) {
+                                        Steps[index] = newStep
+                                        updateStep = ""
+                                    }
+                    if let index = listStepsPhoto.firstIndex(of: updatePhoto) {
+                            if newStepPhoto != nil{
+                                listStepsPhoto[index] = newStepPhoto!
+                                updatePhoto = nil
+                            }
+                        showingUpdateSheet.toggle()
+                                    }
+                }
             }) {
                 Text("Save")
                     .foregroundColor(Color.theme.WhiteInstance)
@@ -116,6 +196,17 @@ private extension CreateStepsView{
                         .font(.custom("ZillaSlab-Regular", size: 22))
                     Spacer()
                     Button(action: {
+                        if let photo = listStepsPhoto[index] ?? nil{
+                               updateStepFunction(step: Steps[index], photo: photo)
+                        }
+                    }) {
+                        Image(systemName: "pencil.circle")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(Color.theme.OrangeInstance)
+                            .padding(.trailing, 15)
+                    }
+                    Button(action: {
                         // Delete the step at the current index
                         Steps.remove(at: index)
                         // Delete the corresponding step photo
@@ -123,17 +214,17 @@ private extension CreateStepsView{
                     }) {
                         Image(systemName: "minus.circle")
                             .resizable()
-                            .frame(width: 25, height: 25)
+                            .frame(width: 30, height: 30)
                             .foregroundColor(Color.theme.OrangeInstance)
-                            .padding(.trailing, 20)
+                            .padding(.trailing, 15)
                     }
                 }.frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 20)
+                    .padding(.leading, 10)
                 HStack {
                     Circle().fill(Color.theme.OrangeInstance).frame(width: 10, height: 10)
                     Text(step)
                         .font(.custom("ZillaSlab-Regular", size: 20))
-                        .frame(width: 280, alignment: .leading)
+                        .frame(width: 300, alignment: .leading)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.leading, 20)
